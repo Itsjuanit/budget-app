7982db13c53f952caea11efba57e40a7a0b9635c
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2024-11-21 15:57:12 -0300

    Naming

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
                   <div className="flex items-center">
-                    <span className="text-xl font-bold text-gray-800">Paga Todo</span>
+                    <span className="text-xl font-bold text-gray-800">PAGATODO</span>
                   </div>
                   <AuthActions />
                 </div>
